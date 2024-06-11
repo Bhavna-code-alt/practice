99d97ff941cb72fb310b73684f0c96fde79c471c
Bhavna-code-alt <bhavnavaishnav0402@gmail.com>
2024-06-11 17:48:45 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,9 @@
 console.log("My name is Bhavna");
 const lastName = "Vaishnav";
 console.log("My name is Bhavna " + lastName);
+const age = 23;
+if(age > 21) {
+  console.log("You can Drive!");
+} else {
+  console.log("Sorry! You cannot Drive");
+}
